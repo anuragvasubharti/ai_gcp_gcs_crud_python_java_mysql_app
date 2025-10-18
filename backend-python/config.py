86596becc034935460d5083661f52948b6dcb9fc
anuragvasubharti <anuragvasubharti@gmail.com>
2025-10-18 16:12:32 +0530

    Removed

--- backend-python/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-MYSQL_CONFIG = {
-    "host": "localhost",
-    "user": "gcs_user",
-    "password": "admin",
-    "database": "ai_gcp_gcs",
-    "auth_plugin": "mysql_native_password"
-}
